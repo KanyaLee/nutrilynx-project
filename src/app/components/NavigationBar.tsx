f8356b80c02bbd6716ecc8c7637ef1f43189693e
lynnda <kl986@cornell.edu>
2024-05-01 00:17:37 -0400

    working on navigation bar

--- src/app/components/NavigationBar.tsx
+++ src/app/components/NavigationBar.tsx
@@ -4,15 +4,14 @@ import Link from 'next/link';
 import Image from 'next/image';
 import React from 'react';
 import styles from  './NavigationBar.module.css';
-import PinDropIcon from '@mui/icons-material/PinDrop';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 const NavigationBar: React.FC = () => {
   return (
     <nav className={styles.navbar}>
         <div className={styles.logo}>
-            <Link href="/"><Image src="/images/nutrilynx-logo.png" alt="" width={50} height={50} ></Image></Link>
+            <Link href="/home"><Image src="/images/nutrilynx-logo.png" alt="" width={50} height={50} ></Image></Link>
         </div>
         <div className={styles.links}>
             <Link href="/about-us">About Us</Link>
@@ -22,7 +21,7 @@ const NavigationBar: React.FC = () => {
             <Link href="/assessment">Assessment</Link>
         </div>
         <div className={styles.icons}>
-            <Link href="/locations"><PinDropIcon /></Link>
+            <Link href="/locations"><LocationOnOutlinedIcon /></Link>
             <Link href="/auth"><AccountCircleIcon /></Link>
             <Link href="/cart"><ShoppingCartIcon /></Link>
         </div>
---
+++ src/pages/home/HeroSection.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HeroSection:React.FC = () => { 
+    return <h1>Hero Section</h1>
+}
+
+export default HeroSection
